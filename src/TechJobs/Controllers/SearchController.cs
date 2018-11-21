81dda79dc931462975e96008ef65eb07a274e664
Ciro10 <mtmti@ymail.com>
2018-11-20 23:41:17 -0500

    final

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -15,6 +15,23 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
-
+        public IActionResult Results(string searchTerm)
+        {
+            bool C4 = string.IsNullOrEmpty(searchTerm);
+            if (C4 == true)
+            {
+                ViewBag.columns = ListController.columnChoices;
+                ViewBag.title = "Results";
+                ViewBag.keyword = "empty";
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.columns = ListController.columnChoices;
+                ViewBag.title = "Results";
+                ViewBag.jobs = JobData.FindByValue(searchTerm);
+                return View("Index");
+            }       
+        }
     }
 }
